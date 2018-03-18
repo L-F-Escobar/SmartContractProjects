8660026eed8a4d2a1d2de70286bc31a123400bd7
Luis F. Escobar-Driver <lfescoba@uci.edu>
2018-03-18 12:17:12 -0700

    fixed some bugs + comments

--- Skeletor/contracts/skeletorFactory.sol
+++ Skeletor/contracts/skeletorFactory.sol
@@ -1,28 +1,23 @@
 pragma solidity ^0.4.18;
 
-import "zeppelin-solidity/contracts/token/ERC721/ERC721.sol";
-import "zeppelin-solidity/contracts/ownership/Ownable.sol";
-import "zeppelin-solidity/contracts/math/SafeMath.sol";
+import "./zeppelin-solidity/contracts/token/ERC721/ERC721.sol";
+import "./zeppelin-solidity/contracts/ownership/Ownable.sol";
+import "./zeppelin-solidity/contracts/math/SafeMath.sol";
 
-
-contract SkeletorFactory is ERC721, Ownable {
-    // Using SageMath contract for data type - these data types will be able to inherate from contract.
+/// @title SkeletorFactor
+/// @author LD2045
+/// @dev This contract will form the base layer (r00t) of an attempt to make a game like World of Warcraft on Etheruem using the ERC721 standard. 
+contract SkeletorFactory is Ownable {
+    /// @notice Declarations using SafeMath. These datatypes will be able to inherit contract.
     using SafeMath for uint256;
-    using SafeMath32 for uint32;
-    using SafeMath16 for uint16;
+    using SafeMath for uint32;
+    using SafeMath for uint16;
 
     uint coowlDown = 1 days;
 
-    event NewSkeleton(bool engaged,
-                    string name,
+    event NewSkeleton(string name,
                     string charType,
-                    uint dna,
-                    uint readyTime,
-                    uint16 wins,
-                    uint16 losses,
-                    uint16 totalHealth,
-                    uint16 totalMana,
-                    mapping (string => uint16) weapons);
+                    uint dna);
 
     struct Skeleton {
         bool engaged;
@@ -38,6 +33,6 @@ contract SkeletorFactory is ERC721, Ownable {
         mapping (string => uint16) weapons;
     }
 
-    // An array(vector) of Skeletons. 
+    /// @notice An array(vector) of Skeletons. 
     Skeleton[] public skeletons;
 }
