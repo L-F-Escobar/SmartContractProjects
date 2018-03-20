51858fb8d07108f31ff7f9722627f9dc6f3ffa65
Luis F. Escobar-Driver <ldriver@hotbsoftware.com>
2018-03-20 15:51:57 -0700

    first 2 contracts are done

--- Skeletor/contracts/CharacterItems.sol
+++ Skeletor/contracts/CharacterItems.sol
@@ -2,25 +2,30 @@ pragma solidity ^0.4.18;
 
 import "./CharacterFactory.sol";
 
+
+/// @dev This contract introduces drop mechanics. Drops & their rarity are calculated and returned with the internal functions below.
+/// @notice Every internal function returns its drop/rarity.
 contract CharacterItems is CharacterFactory {
     /// @dev enums are explicitly convertible to and from all integer types but implicit conversion is not allowed.
     enum Armour {Chest, Helm, Boots, Leggings, Gloves, Shield} // 0,1,2,3,4,5
     enum Weapon {Sword, Axe, Wand, Gun, Hammer, Fist} // 0,1,2,3,4,5
     /// @notice <Left to right> <Common to rare>
-    enum Rarity {Grey, Blue, DarkBlue, Purple} // 0,1,2,3
+    enum Rarity {White, LightBlue, DarkBlue, Purple, Orange} // 0,1,2,3,4
 
     /// @dev Returns the rarity of the weapon dropped. 
     function _calcWeaponRarity() internal returns(Rarity) {
         /// @dev Will return the last 2 numbers (00-99) of a rand num.
         uint8 rarityNumber = uint8(_generateRandomness(100));
         if (rarityNumber == 0) { // 1% chance.
-            return Rarity.Purple;
-        } else if (rarityNumber > 0 && rarityNumber <= 7) { // 7% chance.
+            return Rarity.Orange;
+        } else if (rarityNumber > 0 && rarityNumber <= 5) { // 5% chance.
+            return Rarity.Purple; 
+        } else if (rarityNumber > 5 && rarityNumber <= 15) { // 10% chance.
             return Rarity.DarkBlue; 
-        } else if (rarityNumber > 7 && rarityNumber <= 45) { // 38% chance.
-            return Rarity.Blue; 
-        } else {    // 54% chance.
-            return Rarity.Grey;
+        } else if (rarityNumber > 15 && rarityNumber <= 49) { // 34% chance.
+            return Rarity.LightBlue; 
+        } else {    // 50% chance.
+            return Rarity.White;
         }
     }
 
