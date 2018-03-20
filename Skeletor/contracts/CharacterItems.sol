fcb68d815646d2e06675228bc1ed770d9d317e91
Luis F. Escobar-Driver <ldriver@hotbsoftware.com>
2018-03-20 15:25:12 -0700

    armour drop function

--- Skeletor/contracts/CharacterItems.sol
+++ Skeletor/contracts/CharacterItems.sol
@@ -4,11 +4,10 @@ import "./CharacterFactory.sol";
 
 contract CharacterItems is CharacterFactory {
     /// @dev enums are explicitly convertible to and from all integer types but implicit conversion is not allowed.
-    enum Armour {Chest, Helm, Boots, Leggings, Gloves, Shield}
-    enum Weapon {Sword, Axe, Wand, Gun, Hammer, Fist}
-    enum CharType {Orc, Elf, Human, Dwarve}
+    enum Armour {Chest, Helm, Boots, Leggings, Gloves, Shield} // 0,1,2,3,4,5
+    enum Weapon {Sword, Axe, Wand, Gun, Hammer, Fist} // 0,1,2,3,4,5
     /// @notice <Left to right> <Common to rare>
-    enum Rarity {Grey, Blue, DarkBlue, Purple} 
+    enum Rarity {Grey, Blue, DarkBlue, Purple} // 0,1,2,3
 
     /// @dev Returns the rarity of the weapon dropped. 
     function _calcWeaponRarity() internal returns(Rarity) {
@@ -41,6 +40,25 @@ contract CharacterItems is CharacterFactory {
             return Weapon.Hammer;
         } else { // 26% chance.
             return Weapon.Fist;
+        }
+    }
+
+    /// @dev Returns the armour type that is dropped.
+    function _calcArmourDrop() internal returns(Armour) {
+        /// @dev Will return the last 2 numbers (00-99) of a rand num.
+        uint8 armourNumber = uint8(_generateRandomness(100));
+        if (armourNumber < 15) { // 14% chance.
+            return Armour.Chest;
+        } else if (armourNumber > 15 && armourNumber <= 30) { // 15% chance.
+            return Armour.Helm;
+        } else if (armourNumber > 30 && armourNumber <= 45) { // 15% chance.
+            return Armour.Boots;
+        } else if (armourNumber > 45 && armourNumber <= 60) { // 15% chance.
+            return Armour.Leggings;
+        } else if (armourNumber > 60 && armourNumber <= 75) { // 15% chance.
+            return Armour.Gloves;
+        } else { // 26% chance.
+            return Armour.Shield;
         }
     }
 }
