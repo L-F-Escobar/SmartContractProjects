d58d7591a7181a7b89d2e6513c975a39f482ea0f
Luis F. Escobar-Driver <ldriver@hotbsoftware.com>
2018-03-20 13:06:03 -0700

    symantics

--- Skeletor/contracts/CharacterItems.sol
+++ Skeletor/contracts/CharacterItems.sol
@@ -4,13 +4,13 @@ import "./CharacterFactory.sol";
 
 contract CharacterItems is CharacterFactory {
     /// @dev enums are explicitly convertible to and from all integer types but implicit conversion is not allowed.
-    enum ArmourTypes {Chest, Helm, Boots, Leggings, Gloves, Shield}
-    enum WeaponTypes {Sword, Axe, Wand, Gun, Hammer, Fist}
+    enum Armour {Chest, Helm, Boots, Leggings, Gloves, Shield}
+    enum Weapons {Sword, Axe, Wand, Gun, Hammer, Fist}
     enum CharType {Orc, Elf, Human, Dwarve}
     /// @notice <Left to right> <Common to rare>
     enum RareColor {Grey, Blue, DarkBlue, Purple} 
-    ArmourTypes armour;
-    WeaponTypes weapon;
-    CharType charPick;
-    RareColor colorTracker;
+    Armour armourItem;
+    Weapons weaponItem;
+    CharType characterType;
+    RareColor itemRarity;
 }
