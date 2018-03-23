1bf1c8ecaa33752219b65e27078ad3d2b9c822cf
Luis F. Escobar-Driver <lfescoba@uci.edu>
2018-03-23 10:11:35 -0700

    Auto stash before merge of "master" and "origin/master"

--- Tutorials/ETHPetShop/contracts/Migrations.sol
+++ Tutorials/ETHPetShop/contracts/Migrations.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.4.2;
 
 contract Migrations {
-  address public owner;
-  uint public last_completed_migration;
+    address public owner;
+    uint public last_completed_migration;
 
   modifier restricted() {
     if (msg.sender == owner) _;
